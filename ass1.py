d41b700254fb5544e9598e0e61e88cb30ebee656
nullpointertong <auz.richardt@gmail.com>
2019-04-16 16:27:58 +1000

    Fixed encoding error

--- ass1.py
+++ ass1.py
@@ -1,30 +1,24 @@
 #import socket module
-import http.server
 from socket import *
-import socket as Socket
 import sys # In order to terminate the program
 
-serverSocket = socket(AF_INET, SOCK_STREAM)
-hostname = Socket.gethostname()
-socketNum = 80 
-serverSocket.bind((Socket.gethostbyname(hostname), socketNum))
 
-serverSocket.listen(1)
+
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a sever socket
 #Fill in start
 #Fill in end
-
-while True:    
+while True:
     #Establish the connection
-    print('Ready to serve... IP = ' + Socket.gethostbyname(hostname))
-    connectionSocket, addr = serverSocket.accept()      
+    print('Ready to serve...')
+    connectionSocket, addr =   #Fill in start              #Fill in end          
     try:
-        message = connectionSocket.recv(1024)  #Fill in start          #Fill in end               
+        message =   #Fill in start          #Fill in end               
         filename = message.split()[1]                 
         f = open(filename[1:])                        
-        outputdata = f.read() #Fill in start       #Fill in end                   
+        outputdata = #Fill in start       #Fill in end                   
         #Send one HTTP header line into socket
-        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
         #Fill in start
         #Fill in end                
         #Send the content of the requested file to the client
@@ -35,12 +29,12 @@ while True:
         connectionSocket.close()
     except IOError:
         #Send response message for file not found
-        print("HelloWorld.html is not found in the same directory")
         #Fill in start        
         #Fill in end
         #Close client socket
-        connectionSocket.close()
         #Fill in start
         #Fill in end            
 serverSocket.close()
 sys.exit()#Terminate the program after sending the corresponding data                                    
+
+
