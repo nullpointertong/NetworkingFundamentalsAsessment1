c407478d7cc965b8c1cc89ca2c4e75a31e92650d
opaditya <37282904+opaditya@users.noreply.github.com>
2019-04-25 17:04:37 +1000

    richard carry

--- Assessment1Client.py
+++ Assessment1Client.py
@@ -13,23 +13,21 @@ clientSocket.connect((serverName,serverPort))
 serverPort = "80"
 
 
-
-header = {
-	"first_header" : "GET /%s HTTP/1.1" %(filename),
-	"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-	"Accept-Language": "en-us",
-	"Host": serverName+ ":" + serverPort,
+try:
+	clientSocket = socket(AF_INET, SOCK_STREAM)
+	clientSocket.connect((hostname,int(serverName)))
+	header = {
+		"first_header" : "GET /%s HTTP/1.1" %(filename),
+		"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+		"Accept-Language": "en-us",
+		"Host": serverName+ ":" + serverPort,
 	}
+	http_header = "\r\n".join("%s:%s" %(item,header[item]) for item in header)
+	print (http_header)
+	clientSocket.send("%s\r\n\r\n" %(http_header))
 
-serverPort = 80   
-
-http_header = "\r\n".join("%s:%s" %(item,header[item]) for item in header)
-print (http_header)
-
-http =  http_header + "\r\n\r\n"
-
-clientSocket.send(http.encode())
-
+except IOError:
+	sys.exit()
 
 httpV = clientSocket.recv(1024)
 
