5e31db15a84dc4c4d04caf90d66faf0ae56d2d9b
nullpointertong <auz.richardt@gmail.com>
2019-04-25 15:59:51 +1000

    Added HTTP Header

--- Assessment1Client.py
+++ Assessment1Client.py
@@ -3,18 +3,35 @@ import socket as Socket
 
 
 hostname = Socket.gethostname()
-serverName = Socket.gethostbyname(hostname)
-filename = "HelloWorld.html"
+serverName = Socket.gethostbyname(hostname)  
+filename = "HelloWorl.html"
 serverPort = 80
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 
-string = "HTTP/1.1/" + filename + " 200 OK\r\n\r\n"
+serverPort = "80"
 
-clientSocket.send(string.encode())
+
+
+header = {
+	"first_header" : "GET /%s HTTP/1.1" %(filename),
+	"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+	"Accept-Language": "en-us",
+	"Host": serverName+ ":" + serverPort,
+	}
+
+serverPort = 80   
+
+http_header = "\r\n".join("%s:%s" %(item,header[item]) for item in header)
+print (http_header)
+
+http =  http_header + "\r\n\r\n"
+
+clientSocket.send(http.encode())
+
+
 httpV = clientSocket.recv(1024)
-
 
 print ('From Server : ',httpV.decode())
 clientSocket.close()
